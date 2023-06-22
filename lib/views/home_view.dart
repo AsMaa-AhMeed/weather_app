2d0c4216991eb3808b3b39c2b54231972c5ee236
AsMaa-AhMeed <ssemo8385@gmail.com>
2023-06-22 16:32:01 +0300

    first commit

---
+++ lib/views/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'there is no weather 😔 start',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              'searching now 🔍',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
